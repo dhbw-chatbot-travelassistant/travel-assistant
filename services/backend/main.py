6b3634bf00eec788608b7dcdb9fcfa6ab0d9f8d9
ulfi03 <marius.ulfers@outlook.de>
2025-04-07 11:45:20 +0200

    Fix startup of backend and frontend

--- services/backend/main.py
+++ services/backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from data.LLM_connection import get_hotel_recommendations
+from LLM_connection import get_hotel_recommendations
 
 app = FastAPI()
 
